aeffe264091462ff968ecd39f111785246c74d8a
evanfuture <evan@evanpayne.com>
2021-12-04 09:21:45 +0100

    day03b answered

--- libs/year2021/day03/src/lib/methods.spec.ts
+++ libs/year2021/day03/src/lib/methods.spec.ts
@@ -11,7 +11,7 @@ describe('year2021Day03', () => {
 
   it('should work for example B', () => {
     const inputB = transformInput(exampleAText);
-    const answerB = 0;
+    const answerB = 230;
     expect(methodB(inputB)).toEqual(answerB);
   });
 });
--- libs/year2021/day03/src/lib/methods.ts
+++ libs/year2021/day03/src/lib/methods.ts
@@ -1,4 +1,4 @@
-export function methodA(input: string[]): number {
+function common(input: string[]): Record<number, number>[] {
   const columnStart: number[][] = input[0].split('').map(() => []);
 
   const counts = input.reduce((acc, curr) => {
@@ -6,12 +6,15 @@ export function methodA(input: string[]): number {
     return acc.map((place, i) => [...place, Number(digits[i])]);
   }, columnStart);
 
-  const splits = counts.map((count) =>
+  return counts.map((count) =>
     count.reduce((acc, curr) => {
       return { ...acc, [curr]: (acc[curr] || 0) + 1 };
     }, {} as Record<number, number>),
   );
+}
 
+export function methodA(input: string[]): number {
+  const splits = common(input);
   const { g, e } = splits.reduce(
     (acc: { g: string; e: string }, curr) => {
       const g = curr[0] >= curr[1] ? '0' : '1';
@@ -30,6 +33,36 @@ export function methodA(input: string[]): number {
   return powerConsumption;
 }
 
-export function methodB(input: any[]): number {
-  return 0;
+export function methodB(input: string[]): number {
+  const columnStart: number[][] = input[0].split('').map(() => []);
+
+  const { o, c } = columnStart.reduce(
+    (acc: { o: string[]; c: string[] }, _curr, i) => {
+      let newO = acc.o;
+      let newC = acc.c;
+      if (newO.length > 1) {
+        const oSplits = common(newO);
+        const g = oSplits[i][0] > oSplits[i][1] ? '0' : '1';
+        newO = newO.filter((item) => item.split('')[i] === g);
+      }
+      if (newC.length > 1) {
+        const cSplits = common(newC);
+        const e = cSplits[i][0] > cSplits[i][1] ? '1' : '0';
+        newC = newC.filter((item) => item.split('')[i] === e);
+      }
+      return {
+        o: newO,
+        c: newC,
+      };
+    },
+    {
+      o: input,
+      c: input,
+    },
+  );
+
+  const o2 = parseInt(o[0], 2);
+  const co2 = parseInt(c[0], 2);
+  const powerConsumption = o2 * co2;
+  return powerConsumption;
 }
